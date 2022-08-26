0cf4b95b78751bedf85f95a27acd77ae89a0f0dd
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 04:02:23 +0600

    updated appointment-config.module

--- src/app/appointment-config/appointment-config.module.ts
+++ src/app/appointment-config/appointment-config.module.ts
@@ -6,12 +6,46 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from '../shared/shared.module';
 import { AppointmentConfigComponent } from './components/appointment-config/appointment-config.component';
 import { CreateAppointmentComponent } from './components/create-appointment/create-appointment.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatNativeDateModule, MAT_DATE_FORMATS} from '@angular/material/core';
 
-const MATERIAL_MODULES = [MatDialogModule, MatCardModule, MatButtonModule];
+const MATERIAL_MODULES = [
+  MatDialogModule,
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [AppointmentConfigComponent, CreateAppointmentComponent],
-  imports: [CommonModule, MATERIAL_MODULES, SharedModule],
-  exports: [AppointmentConfigComponent],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, MATERIAL_MODULES, NgbModule],
+  providers: [
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: {
+        parse: {
+          dateInput: ['l', 'LL'],
+        },
+        display: {
+          dateInput: 'L',
+          monthYearLabel: 'MMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+        },
+      },
+    },
+  ],
+  exports: [AppointmentConfigComponent, CreateAppointmentComponent],
 })
 export class AppointmentConfigModule {}
