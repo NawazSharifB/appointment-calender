648fe87b7ae5c6416c8cd024b602a43614a88a37
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 04:03:55 +0600

    added selection dropdown

---
+++ src/app/appointment-config/constants/meridiem-selection.ts
@@ -0,0 +1,6 @@
+import { Meridiem } from '../../shared/enums/meridiem';
+
+export const meridiemSelection = [
+  {name: 'AM', value: Meridiem.AM},
+  {name: 'PM', value: Meridiem.PM},
+];
---
+++ src/app/appointment-config/constants/minute-selection.ts
@@ -0,0 +1,6 @@
+export const minuteSelection = [
+  {name: '00', value: 0},
+  {name: '15', value: 15},
+  {name: '30', value: 30},
+  {name: '45', value: 45},
+];
