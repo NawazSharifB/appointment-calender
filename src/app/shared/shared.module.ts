134d651d59ea49fdbd5f7748c280501bc6ad4c69
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 04:05:59 +0600

    updated shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FlexLayoutModule],
+  exports: [FlexLayoutModule],
 })
 export class SharedModule {}
