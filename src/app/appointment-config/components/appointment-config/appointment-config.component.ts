1f36e4fa7fb46a78c6ae8fcacc541f3befeeac79
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 21:22:50 +0600

    added functionality to change month

--- src/app/appointment-config/components/appointment-config/appointment-config.component.ts
+++ src/app/appointment-config/components/appointment-config/appointment-config.component.ts
@@ -1,16 +1,34 @@
+import { RoutePaths } from './../../../enums/route-paths';
+import { monthSelection } from './../../constants/month-selection';
 import { DialogService } from '../../../shared/services/dialog.service';
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { MonthSelectionValues } from '../../enums/month-values';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointment-config',
   templateUrl: './appointment-config.component.html',
   styleUrls: ['./appointment-config.component.scss']
 })
-export class AppointmentConfigComponent implements OnDestroy {
+export class AppointmentConfigComponent implements OnInit, OnDestroy {
+  monthSelection = monthSelection;
+  monthSelectionControl = new FormControl(MonthSelectionValues.January);
+
   private subscription$ = new Subscription();
 
-  constructor(private dialogService: DialogService) {}
+  constructor(private dialogService: DialogService, private router: Router) {}
+
+  ngOnInit(): void {
+      this.subscription$.add(
+        this.monthSelectionControl.valueChanges.subscribe({
+          next: value => {
+            this.router.navigate([RoutePaths.Month, value]);
+          }
+        })
+      );
+  }
 
   ngOnDestroy(): void {
       this.subscription$.unsubscribe();
