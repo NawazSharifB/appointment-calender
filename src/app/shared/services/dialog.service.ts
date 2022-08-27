fe754fa4a5e00e83bdbe53e8ff80f45fdfa83286
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-28 03:19:37 +0600

    format dialog.service

--- src/app/shared/services/dialog.service.ts
+++ src/app/shared/services/dialog.service.ts
@@ -1,12 +1,12 @@
-import { AppointmentDetailsComponent } from './../../calender/components/appointment-details/appointment-details.component';
-import { AppointmentData } from './../interfaces/appointment-data';
-import { Observable } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
 import { Injectable } from '@angular/core';
-import { CreateAppointmentComponent } from '../../appointment-config/components/create-appointment/create-appointment.component';
-import { tap } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CreateAppointmentComponent } from '../../appointment-config/components/create-appointment/create-appointment.component';
+import { AppointmentDetailsComponent } from '../../calender/components/appointment-details/appointment-details.component';
 import { ToastMessages } from '../constants/toast-messages';
+import { AppointmentData } from '../interfaces/appointment-data';
 
 @Injectable({
   providedIn: 'root',
@@ -17,22 +17,26 @@ export class DialogService {
   constructor(private matDialog: MatDialog, private snackbar: MatSnackBar) {}
 
   openCreateAppointmentForm(): Observable<boolean> {
-    const dialogRef = this.matDialog.open(CreateAppointmentComponent, {width: this.dialogBoxWidth, panelClass: ['detail-container']});
+    const dialogRef =
+      this.matDialog.open(
+        CreateAppointmentComponent,
+        { width: this.dialogBoxWidth, panelClass: ['detail-container'] },
+      );
 
     return dialogRef.afterClosed().pipe(
       tap(hasCreatedAppointment => {
         if (hasCreatedAppointment) {
-          this.snackbar.open(ToastMessages.AppointmentCreationSuccessful, '', {duration: 2500})
+          this.snackbar.open(ToastMessages.AppointmentCreationSuccessful, '', { duration: 2500 })
         }
       })
     );
   }
 
-  openAppointmentInfoDialog(appointmentInfo: AppointmentData): Observable<boolean> {
+  openAppointmentInfoDialog(appointmentInfo: AppointmentData): Observable<void> {
     const dialogRef =
       this.matDialog.open(
         AppointmentDetailsComponent,
-        {width: this.dialogBoxWidth, data: appointmentInfo, panelClass: ['detail-container']},
+        { width: this.dialogBoxWidth, data: appointmentInfo, panelClass: ['detail-container'] },
       );
 
     return dialogRef.afterClosed();
