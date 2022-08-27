a7e6a13a9549a0502f235370708a0a07330d31ee
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 14:02:34 +0600

    updated calender module

--- src/app/calender/calender.module.ts
+++ src/app/calender/calender.module.ts
@@ -1,11 +1,16 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from './../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
-import { CalenderBodyComponent } from "./components/calender-body/calender-body.component";
+import { CalenderBodyComponent } from './components/calender-body/calender-body.component';
+import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
+import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
 
+const MATERIAL_MODULES = [MatCardModule, MatButtonModule];
 @NgModule({
-  declarations: [CalenderBodyComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [CalenderBodyComponent, AppointmentListComponent, AppointmentDetailsComponent],
+  imports: [CommonModule, SharedModule, MATERIAL_MODULES],
   exports: [CalenderBodyComponent],
 })
 export class CalenderModule {}
