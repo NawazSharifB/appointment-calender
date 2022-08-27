94e0e4316ee260b79bf974a4b175cbf959146411
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-28 02:32:01 +0600

    changed variable name to activatedRoute

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,11 +1,11 @@
-import { RoutePaths } from './../enums/route-paths';
-import { RouteParam } from './../enums/route-param';
-import { Subscription } from 'rxjs';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
 import { getYear, startOfToday } from 'date-fns';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { MonthSelectionValues } from '../appointment-config/enums/month-values';
+import { RouteParam } from '../enums/route-param';
+import { RoutePaths } from '../enums/route-paths';
 
 @Component({
   selector: 'app-calender',
@@ -14,13 +14,14 @@ import { MonthSelectionValues } from '../appointment-config/enums/month-values';
 })
 export class CalenderComponent implements OnInit, OnDestroy {
   dateToShowCalender = Date.now();
+
   private subscription$ = new Subscription();
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.subscription$.add(
-      this.route.paramMap.pipe(
+      this.activatedRoute.paramMap.pipe(
         filter(param => {
           if (param.has(RouteParam.NumberOfTheMonth)) {
             return true;
