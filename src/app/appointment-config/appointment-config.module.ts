ec676b1e626c274e009f106f7fb54c4c9a865be5
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 19:21:38 +0600

    updated modules and exports for lazy loading

--- src/app/appointment-config/appointment-config.module.ts
+++ src/app/appointment-config/appointment-config.module.ts
@@ -2,28 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from '../shared/shared.module';
 import { AppointmentConfigComponent } from './components/appointment-config/appointment-config.component';
 import { CreateAppointmentComponent } from './components/create-appointment/create-appointment.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatIconModule} from '@angular/material/icon';
-import {MatNativeDateModule, MAT_DATE_FORMATS} from '@angular/material/core';
 
 const MATERIAL_MODULES = [
-  MatDialogModule,
   MatCardModule,
   MatButtonModule,
   MatInputModule,
   MatFormFieldModule,
   MatSelectModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
   MatIconModule,
 ];
 
@@ -31,23 +23,7 @@ const EXPORTED_DECLARATIONS = [AppointmentConfigComponent];
 
 @NgModule({
   declarations: [CreateAppointmentComponent, EXPORTED_DECLARATIONS],
-  imports: [CommonModule, ReactiveFormsModule, SharedModule, MATERIAL_MODULES, NgbModule],
-  providers: [
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: {
-        parse: {
-          dateInput: ['l', 'LL'],
-        },
-        display: {
-          dateInput: 'L',
-          monthYearLabel: 'MMM YYYY',
-          dateA11yLabel: 'LL',
-          monthYearA11yLabel: 'MMMM YYYY',
-        },
-      },
-    },
-  ],
+  imports: [CommonModule, SharedModule, MATERIAL_MODULES],
   exports: [EXPORTED_DECLARATIONS],
 })
 export class AppointmentConfigModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,27 @@
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatNativeDateModule, MAT_DATE_FORMATS} from '@angular/material/core';
+
+const MODULES_TO_EXPORT = [
+  MatDialogModule,
+  FlexLayoutModule,
+  ReactiveFormsModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+];
 
 @NgModule({
   declarations: [PageNotFoundComponent],
-  imports: [CommonModule, FlexLayoutModule],
-  exports: [FlexLayoutModule, PageNotFoundComponent],
+  imports: [CommonModule, MODULES_TO_EXPORT],
+  exports: [PageNotFoundComponent, MODULES_TO_EXPORT],
 })
 export class SharedModule {}
