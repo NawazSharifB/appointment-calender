0c2b275eb44d792a4d27c8a31aa047b0d980d658
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-28 02:23:33 +0600

    should redirect to current month as default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoutePaths } from './enums/route-paths';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
-
-const NUMBER_OF_JANUARY_MONTH = '1';
+import { getCurrentMonthString } from './utils/date-utilities';
 
 const routes: Routes = [
   { path: RoutePaths.Month, loadChildren: () => import('../app/calender/calender.module').then(m => m.CalenderModule) },
   { path: RoutePaths.NotFound, component: NotFoundComponent },
-  { path: RoutePaths.RootRoute, redirectTo: `/${RoutePaths.Month}/${NUMBER_OF_JANUARY_MONTH}`, pathMatch: 'full' },
+  { path: RoutePaths.RootRoute, redirectTo: `/${RoutePaths.Month}/${getCurrentMonthString()}`, pathMatch: 'full' },
   { path: RoutePaths.WildCard, component: PageNotFoundComponent },
 ];
 
--- src/app/calender/calender-routing.module.ts
+++ src/app/calender/calender-routing.module.ts
@@ -2,13 +2,12 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { CalenderComponent } from './calender.component';
 import { RouteParam } from '../enums/route-param';
-
-const NUMBER_OF_JANUARY_MONTH = '1';
+import { getCurrentMonthString } from '../utils/date-utilities';
 
 @NgModule({
   imports: [RouterModule.forChild([
     { path: `:${RouteParam.NumberOfTheMonth}`, component: CalenderComponent, pathMatch: 'full'},
-    { path: ``, redirectTo: NUMBER_OF_JANUARY_MONTH, pathMatch: 'full'},
+    { path: ``, redirectTo: getCurrentMonthString(), pathMatch: 'full'},
   ])],
   exports: [RouterModule],
 })
