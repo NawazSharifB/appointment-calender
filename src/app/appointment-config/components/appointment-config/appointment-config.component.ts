d628b59ffdc1050fe4a71240d2a2da1b4e610a6e
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 21:36:15 +0600

    should get route month on initial load

--- src/app/appointment-config/components/appointment-config/appointment-config.component.ts
+++ src/app/appointment-config/components/appointment-config/appointment-config.component.ts
@@ -1,3 +1,4 @@
+import { RouteParam } from './../../../enums/route-param';
 import { RoutePaths } from './../../../enums/route-paths';
 import { monthSelection } from './../../constants/month-selection';
 import { DialogService } from '../../../shared/services/dialog.service';
@@ -5,7 +6,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { MonthSelectionValues } from '../../enums/month-values';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointment-config',
@@ -14,20 +15,16 @@ import { Router } from '@angular/router';
 })
 export class AppointmentConfigComponent implements OnInit, OnDestroy {
   monthSelection = monthSelection;
-  monthSelectionControl = new FormControl(MonthSelectionValues.January);
+  monthSelectionControl!: FormControl;
 
   private subscription$ = new Subscription();
 
-  constructor(private dialogService: DialogService, private router: Router) {}
+  constructor(private dialogService: DialogService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-      this.subscription$.add(
-        this.monthSelectionControl.valueChanges.subscribe({
-          next: value => {
-            this.router.navigate([RoutePaths.Month, value]);
-          }
-        })
-      );
+    const currentRoute = this.route.snapshot.paramMap.get(RouteParam.NumberOfTheMonth);
+    this.createSelectionMonthControl(currentRoute);
+    this.changeRouteOnMonthChange();
   }
 
   ngOnDestroy(): void {
@@ -37,6 +34,27 @@ export class AppointmentConfigComponent implements OnInit, OnDestroy {
   openAppointmentFormDialog(): void {
     this.subscription$.add(
       this.dialogService.openCreateAppointmentForm().subscribe(),
+    );
+  }
+
+  private createSelectionMonthControl(currentRouteMonth: string | null): void {
+    let initialMonthControlValue: string = MonthSelectionValues.January;
+    const currentRouteNumber = currentRouteMonth ? +currentRouteMonth : -1;
+
+    if (!isNaN(currentRouteNumber) && currentRouteNumber >=  +MonthSelectionValues.January && currentRouteNumber <=  +MonthSelectionValues.December) {
+      initialMonthControlValue = currentRouteNumber.toString();
+    }
+
+    this.monthSelectionControl = new FormControl(initialMonthControlValue);
+  }
+
+  private changeRouteOnMonthChange() {
+    this.subscription$.add(
+      this.monthSelectionControl.valueChanges.subscribe({
+        next: value => {
+          this.router.navigate([RoutePaths.Month, value]);
+        }
+      })
     );
   }
 }
