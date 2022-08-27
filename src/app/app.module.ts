9896c6a91ce4e67e5b481c7c26a44e6c02f92e94
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-28 02:15:15 +0600

    app.module: removed date-format provider, reorder imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { MAT_DATE_FORMATS } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -15,22 +15,6 @@ import { MAT_DATE_FORMATS } from '@angular/material/core';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-  ],
-  providers: [
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: {
-        parse: {
-          dateInput: ['l', 'LL'],
-        },
-        display: {
-          dateInput: 'L',
-          monthYearLabel: 'MMM YYYY',
-          dateA11yLabel: 'LL',
-          monthYearA11yLabel: 'MMMM YYYY',
-        },
-      },
-    },
   ],
   bootstrap: [AppComponent]
 })
