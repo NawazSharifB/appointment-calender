cf3a6bfda750d8837030ad606e341951e6aea583
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 18:57:04 +0600

    added page-not-found.component in shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
+  declarations: [PageNotFoundComponent],
   imports: [CommonModule, FlexLayoutModule],
-  exports: [FlexLayoutModule],
+  exports: [FlexLayoutModule, PageNotFoundComponent],
 })
 export class SharedModule {}
