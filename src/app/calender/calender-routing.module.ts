8b8a5c312f3f3439879ff5da011dad4934c311ef
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 19:38:32 +0600

    code refactoring

--- src/app/calender/calender-routing.module.ts
+++ src/app/calender/calender-routing.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 import { CalenderComponent } from './calender.component';
 import { RouteParam } from '../enums/route-param';
 
+const NUMBER_OF_JANUARY_MONTH = '1';
+
 @NgModule({
   imports: [RouterModule.forChild([
     { path: `:${RouteParam.NumberOfTheMonth}`, component: CalenderComponent, pathMatch: 'full'},
+    { path: ``, redirectTo: NUMBER_OF_JANUARY_MONTH, pathMatch: 'full'},
   ])],
   exports: [RouterModule],
 })
