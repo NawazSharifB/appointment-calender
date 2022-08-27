92cddff90978657fce5c3bf9f22cae7a722821a0
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-28 04:17:04 +0600

    format

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist/appointment-calender'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/appointment-calender/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname+ '/dist/appointment-calender/index.html')));
+const server = http.createServer(app);
+
+server.listen(port, ()=> console.log('server is running on port ' + port));
