7573fdad4ff7ddc6255605556225d3fcce6dd37a
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2022-08-27 06:07:35 +0600

    added appointment-list.component

---
+++ src/app/calender/components/appointment-list/appointment-list.component.ts
@@ -0,0 +1,17 @@
+import { AppointmentData } from './../../../shared/interfaces/appointment-data';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.scss']
+})
+export class AppointmentListComponent implements OnInit {
+  @Input() appointmentList: AppointmentData[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
